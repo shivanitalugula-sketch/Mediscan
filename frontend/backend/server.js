90565e07a9e79dfb8a3fe8b1ae7b9dc879d6cb2a
Shivani <shivanitalugula@gmail.com>
2026-07-19 20:34:44 +0530

    Prepare backend for deployment

--- frontend/backend/server.js
+++ frontend/backend/server.js
@@ -25,8 +25,7 @@ app.post("/upload", upload.single("prescription"), async (req, res) => {
     message: "Prescription uploaded and enhanced successfully!"
   });
 });
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
